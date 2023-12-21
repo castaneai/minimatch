42fb959ecc71ba690b9654d145f9e9df782e1044
castaneai <castaneai@castaneai.net>
2023-12-21 22:50:58 +0900

    test: add assigned delete timeout test

--- pkg/statestore/redis_test.go
+++ pkg/statestore/redis_test.go
@@ -91,6 +91,50 @@ func TestGetActiveTicketsLimit(t *testing.T) {
 	}
 }
 
+func TestAssignedDeleteTimeout(t *testing.T) {
+	mr := miniredis.RunT(t)
+	store := newTestRedisStore(t, mr.Addr())
+	ctx := context.Background()
+
+	require.NoError(t, store.CreateTicket(ctx, &pb.Ticket{Id: "test1"}))
+	require.NoError(t, store.CreateTicket(ctx, &pb.Ticket{Id: "test2"}))
+	activeTickets, err := store.GetActiveTickets(ctx, defaultFetchTicketsLimit)
+	require.NoError(t, err)
+	require.ElementsMatch(t, ticketIDs(activeTickets), []string{"test1", "test2"})
+
+	_, err = store.GetAssignment(ctx, "test1")
+	require.Error(t, err, ErrAssignmentNotFound)
+	_, err = store.GetAssignment(ctx, "test2")
+	require.Error(t, err, ErrAssignmentNotFound)
+
+	as := &pb.Assignment{Connection: "test-assign"}
+	require.NoError(t, store.AssignTickets(ctx, []*pb.AssignmentGroup{
+		{TicketIds: []string{"test1", "test2"}, Assignment: as},
+	}))
+	for i := 0; i < 3; i++ {
+		as1, err := store.GetAssignment(ctx, "test1")
+		require.NoError(t, err)
+		require.Equal(t, as.Connection, as1.Connection)
+	}
+	for i := 0; i < 3; i++ {
+		as2, err := store.GetAssignment(ctx, "test2")
+		require.NoError(t, err)
+		require.Equal(t, as.Connection, as2.Connection)
+	}
+
+	// assigned delete timeout
+	mr.FastForward(DefaultAssignedDeleteTimeout + 1*time.Second)
+
+	_, err = store.GetTicket(ctx, "test1")
+	require.Error(t, err, ErrTicketNotFound)
+	_, err = store.GetTicket(ctx, "test2")
+	require.Error(t, err, ErrTicketNotFound)
+	_, err = store.GetAssignment(ctx, "test1")
+	require.Error(t, err, ErrAssignmentNotFound)
+	_, err = store.GetAssignment(ctx, "test2")
+	require.Error(t, err, ErrAssignmentNotFound)
+}
+
 func ticketIDs(tickets []*pb.Ticket) []string {
 	var ids []string
 	for _, ticket := range tickets {
